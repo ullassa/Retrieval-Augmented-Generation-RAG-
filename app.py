cbb060f88cf527c2f0c8fdf2a5e0805be9ab66ae
ULLAS S A <184759135+ullassa@users.noreply.github.com>
2025-05-28 18:10:22 +0530

    Update app.py\

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#openai.api_key = "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"
+#openai.api_key = "sk-proj-_STnO9sb4uES7hZiioJeRxfZNaGgzqgYxKZI-VhuREt6mxT3woNsyGpoG6oo-2RqHVcnBdjJET3BlbkFJOZJBD4xcFeGeJICTvdB_zQ5sT2kP9b2x3r1cKjgBlkhjvg90qei-i408qXHjOIXgdzGJbmSegA"
 import os
 import openai
 
